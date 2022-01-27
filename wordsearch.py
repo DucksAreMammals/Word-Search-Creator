ea307ef6f4f857c1cd3d6231666e04e6925bb472
DucskAreMammals <34693858+DucksAreMammals@users.noreply.github.com>
2022-01-27 04:45:01 -0600

    Now checks all possibilities

--- wordsearch.py
+++ wordsearch.py
@@ -13,58 +13,78 @@ def main():
 
     wordsearch = [[None for i in range(width)] for j in range(height)]
 
-    for word in words:
-        placed = False
+    wordsearch = place_word(words, wordsearch)
 
-        while not placed:
-            dir_x = random.randint(-1, 1)
-            dir_y = random.randint(-1, 1)
+    if wordsearch != None:
+        for line in wordsearch:
+            for letter in line:
+                if letter == None:
+                    print("- ", end="")
+                else:
+                    print(letter + " ", end="")
+            print()
+    else:
+        print("Cannot fit")
+
+
+def place_word(wordlist, wordsearch):
+    positions = []
+
+    for dir_x in [-1, 0, 1]:
+        for dir_y in [-1, 0, 1]:
 
             if dir_x == 0 and dir_y == 0:
-                if random.random() > 0.5:
-                    dir_x = random.choice([1, -1])
-                else:
-                    dir_y = random.choice([1, -1])
+                continue
 
             min_x = 0
             min_y = 0
-            max_x = width - 1
-            max_y = height - 1
+            max_x = len(wordsearch[0]) - 1
+            max_y = len(wordsearch) - 1
 
             if dir_x == -1:
-                min_x = len(word) - 1
+                min_x = len(wordlist[0]) - 1
 
             if dir_x == 1:
-                max_x = max_x - len(word) + 1
+                max_x = max_x - len(wordlist[0]) + 1
 
             if dir_y == -1:
-                min_y = len(word) - 1
+                min_y = len(wordlist[0]) - 1
 
             if dir_y == 1:
-                max_y = max_y - len(word) + 1
+                max_y = max_y - len(wordlist[0]) + 1
 
-            x = random.randint(min_x, max_x)
-            y = random.randint(min_y, max_y)
+            for x in range(min_x, max_x + 1):
+                for y in range(min_y, max_y + 1):
+                    positions.append((x, y, dir_x, dir_y))
 
-            can_place = True
-            for i, letter in enumerate(word):
-                if wordsearch[y + i * dir_y][x + i * dir_x] not in [letter, None]:
-                    can_place = False
-                    break
+    random.shuffle(positions)
 
-            if can_place:
-                placed = True
+    for x, y, dir_x, dir_y in positions:
+        can_place = True
+        for i, letter in enumerate(wordlist[0]):
+            if wordsearch[y + i * dir_y][x + i * dir_x] not in [letter, None]:
+                can_place = False
+                break
 
-                for i, letter in enumerate(word):
-                    wordsearch[y + i * dir_y][x + i * dir_x] = letter
+        if can_place:
+            added_wordsearch = wordsearch
+            placed = True
 
-    for line in wordsearch:
-        for letter in line:
-            if letter == None:
-                print("- ", end="")
+            for i, letter in enumerate(wordlist[0]):
+                added_wordsearch[y + i * dir_y][x + i * dir_x] = letter
+
+            if len(wordlist) > 1:
+                returned = place_word(wordlist[1:], added_wordsearch)
+
+                if returned != None:
+                    return(added_wordsearch)
             else:
-                print(letter + " ", end="")
-        print()
+                return added_wordsearch
+
+    if len(wordlist) > 1:
+        wordsearch = place_word(wordlist[1:], wordsearch)
+
+    return None
 
 
 if __name__ == '__main__':
