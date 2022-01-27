556abfbce3a8a6dc72cecc11a92fa74404ae707f
DucskAreMammals <34693858+DucksAreMammals@users.noreply.github.com>
2022-01-27 01:43:18 -0600

    Added reading file and input

--- wordsearch.py
+++ wordsearch.py
@@ -1,5 +1,14 @@
 def main():
-    pass
+    width = int(input("Please enter the width of the wordsearch: "))
+    height = int(input("Please enter the height of the wordsearch: "))
+
+    words = []
+
+    with open("wordsearch.txt", "r") as file:
+        for word in file.readlines():
+            words.append(word.strip())
+
+    wordsearch = []
 
 
 if __name__ == '__main__':
