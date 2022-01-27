f1b8a1a316bf73fe1ef13208895213e8a1b88a5b
DucskAreMammals <34693858+DucksAreMammals@users.noreply.github.com>
2022-01-27 01:53:53 -0600

    Added direction functionality

--- wordsearch.py
+++ wordsearch.py
@@ -1,3 +1,6 @@
+import random
+
+
 def main():
     width = int(input("Please enter the width of the wordsearch: "))
     height = int(input("Please enter the height of the wordsearch: "))
@@ -8,7 +11,24 @@ def main():
         for word in file.readlines():
             words.append(word.strip())
 
-    wordsearch = []
+    wordsearch = [[None for i in range(width)] for j in range(height)]
+
+    for word in words:
+        dir_x = random.randint(-1, 1)
+        dir_y = random.randint(-1, 1)
+
+        if dir_x == 0 and dir_y == 0:
+            if random.random() > 0.5:
+                dir_x = random.choice([1, -1])
+            else:
+                dir_y = random.choice([1, -1])
+
+        print(dir_x, dir_y)
+
+    for line in wordsearch:
+        for letter in line:
+            print(str(letter) + " ", end="")
+        print()
 
 
 if __name__ == '__main__':
